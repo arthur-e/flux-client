5872a47dfbbeed7acbc007cd8e5245b104b4d2c9
K. Arthur Endsley <kaendsle@mtu.edu>
2014-02-27 11:29:27 -0500

    Added MTRI logo

--- app/view/D3Panel.js
+++ app/view/D3Panel.js
@@ -3,11 +3,24 @@ Ext.define('Flux.view.D3Panel', {
     alias: 'widget.d3panel',
     requires: [
         'Ext.Component',
-        'Ext.layout.container.Fit'
+        'Ext.layout.container.Fit',
+        'Ext.toolbar.Item',
+        'Ext.toolbar.Toolbar'
     ],
 
     layout: {
         type: 'fit'
+    },
+
+    bbar: {
+        border: true,
+        style: { borderColor: '#157fcc' },
+        items: ['->', {
+            xtype: 'tbitem',
+            width: 150,
+            height: 45,
+            html: '<a href="http://mtu.edu"><img src="/flux-client/resources/MTRI_logo.png" /></a>'
+        }]
     },
 
     items: {

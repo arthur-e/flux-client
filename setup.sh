f33d421e098dc5b37f48388f951c5892f5519fe6
Michael Billmire <mgbillmi@mtu.edu>
2015-05-11 10:55:38 -0400

    Fixes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 USERNAME=$USER
 BUILD_DIR=/home/$USERNAME/Source/
+EXT_DIR=/var/www/static/extjs
 
 echo "Installing Ruby..."
 sudo apt-get install ruby ruby1.9.1
 
 echo "Downloading ExtJS library..."
-sudo mkdir -p /var/www/static/extjs/
-sudo chown $USERNAME /var/www/static/extjs
-cd /var/www/static/extjs
-wget http://cdn.sencha.com/ext/gpl/ext-4.2.1-gpl.zip
-unzip ext-4.2.1-gpl.zip
-mv ext.4.2.1.883 /var/www/static/extjs/4.2.1
+sudo mkdir -p $EXT_DIR
+sudo chown $USERNAME $EXT_DIR
+(cd $EXT_DIR && wget http://cdn.sencha.com/ext/gpl/ext-4.2.1-gpl.zip)
+(cd $EXT_DIR && unzip ext-4.2.1-gpl.zip)
+mv $EXT_DIR/ext.4.2.1.883 $EXT_DIR/4.2.1
 
 echo "Installing Sencha SDK..."
 cd $BUILD_DIR
-wget http://cdn.sencha.io/sdk-tools/SenchaSDKTools-2.0.0-beta3-linux-x64.run
+sudo wget http://cdn.sencha.io/sdk-tools/SenchaSDKTools-2.0.0-beta3-linux-x64.run
 sudo chmod +x SenchaSDKTools-2.0.0-beta3-linux-x64.run
 sudo ./SenchaSDKTools-2.0.0-beta3-linux-x64.run
 # Graphical installer should start up... Install to /opt/ by default
